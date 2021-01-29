90f5ac3f802c9a023a537459ad9c420957f5aaa6
agustinntarias <gustin444@gmail.com>
2021-01-29 20:37:11 -0300

    Added Alert Reducer, Action & Types

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,14 @@
+import { REMOVE_ALERT, SET_ALERT } from "../actions/types";
+
+const initialState = [];
+
+export default function alert(state = initialState, { type, payload }) {
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
+  }
+}
