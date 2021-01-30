6b2b3d56a90e15f98b2816763774da1991f15438
agustinntarias <gustin444@gmail.com>
2021-01-29 21:03:04 -0300

    Added Alert Component & Action Call

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import store from "./store";
@@ -15,6 +16,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
