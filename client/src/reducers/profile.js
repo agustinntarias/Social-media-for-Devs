4e7f5eeaa96bfadd432fa8280c284c58bbd7a03c
agustinntarias <gustin444@gmail.com>
2021-01-31 19:38:43 -0300

    Added Profile Reducer and Get Current Profile

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,28 @@
+import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  erorr: {},
+};
+
+export default function profile(state = initialState, { type, payload }) {
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
