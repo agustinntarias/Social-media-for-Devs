80a11368b1f39e47eab30e4abf3d357f43876a47
agustinntarias <gustin444@gmail.com>
2021-01-31 22:24:29 -0300

    Added Education and Experience

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,9 @@
-import { CLEAR_PROFILE, GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+import {
+  CLEAR_PROFILE,
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+} from "../actions/types";
 
 const initialState = {
   profile: null,
@@ -11,6 +16,7 @@ const initialState = {
 export default function profile(state = initialState, { type, payload }) {
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
