a47c5fb8a7073f622fa4c5f89aae5595987fb1f7
agustinntarias <gustin444@gmail.com>
2021-02-01 18:47:00 -0300

    Added Back to Profiles and Edit Profile

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import { addExperience } from "../../actions/profile";
 
 const AddExperience = ({ addExperience, history }) => {
@@ -123,4 +124,4 @@ AddExperience.propTypes = {
   addExperience: PropTypes.func.isRequired,
 };
 
-export default connect(null, { addExperience })(AddExperience);
+export default connect(null, { addExperience })(withRouter(AddExperience));
