8df52d927bfa953c5b05cd55fe797fb93d86b8d2
agustinntarias <gustin444@gmail.com>
2021-02-03 02:30:54 -0300

    Implemented Delete Post Functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+import { setAlert } from "./alert";
+import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -22,7 +23,6 @@ export const getPosts = () => async (dispatch) => {
 export const addLike = (id) => async (dispatch) => {
   try {
     const res = await axios.put(`/api/posts/like/${id}`);
-    console.log({ likes: res.data });
     dispatch({
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
@@ -39,12 +39,30 @@ export const addLike = (id) => async (dispatch) => {
 export const removeLike = (id) => async (dispatch) => {
   try {
     const res = await axios.put(`/api/posts/unlike/${id}`);
-    console.log({ likes: res.data });
 
     dispatch({
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+
+    dispatch(setAlert("Post Removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
